e2ac84d38eb6b450e7981d5a524d596c73dfec55
GeForceGroot <tiwarishreyansh007@gmail.com>
2024-04-02 11:52:17 +0530

    commit 12

--- src/components/AcademicImpact.js
+++ src/components/AcademicImpact.js
@@ -1,29 +1,43 @@
 import React from 'react'
-import './bubble.css'
+// import './bubble.css'
 const AcademicImpact = () => {
     return (
         <>
             <section className='hell' style={{ justifyContent: "center", textAlign: 'center', marginTop: '50px', }}>
                 <div className='ok' style={{ display: 'flex', justifyContent: 'center' }}>
                     <h1 style={{ padding: '7px', color: 'rgb(170 51 170 / 82%)' }}>Academic Impact</h1>
-                    <img src="https://cdn-icons-png.flaticon.com/128/3281/3281323.png" width={'50px'} height={'50px'} style={{ marginRight: '20px' }} alt="" />
+                    <img src="https://cdn-icons-png.flaticon.com/128/3281/3281323.png" width={'50px'} height={'50px'} style={{ marginRight: '25px' }} alt="" />
                 </div>
-                <section className='hello' style={{ display: 'flex', justifyContent: "center", marginTop: "125px" }}>
-                    <section className="ball" style={{ marginLeft: '400px' }}>
+                <section className='hello' >
+                    <div className='container' style={{display:'flex'}}>
+                        <div className='col-sm' style={{margin:'50px', opacity:'.95',width:'200px', boxShadow:'inset 0 0 20px #fff, inset 10px 0 46px #eaf5fc,inset 88px 0px 60px #c2d8fe, inset -20px -60px 100px #fde9ea,inset 0 50px 140px #fde9ea, 0 0 90px #fffk',height:"200px",border: "1px solid lightpink", borderRadius:"50%",backgroundImage: 'linear-gradient(to bottom right, #91defe, #99c0f9, #bdb6ec, #d7b3e3, #efb3d5,#f9bccc)' }}>
+                            <p style={{ marginTop: '50px', fontSize: "25px", fontWeight: 'bold', color: "rgb(115 10 115 / 89%)" }}>1571</p>
+                            <p style={{ marginBottom: '25px', fontSize: "25px", fontWeight: 'bold', color: "rgb(115 10 115 / 89%)" }}>Projects Completed</p>
+                        </div>
+                        <div className='col-sm' style={{margin:'50px', width:'200px', boxShadow:'1px 1px 2px 2px lightpink',height:"200px",border: "1px solid lightpink", borderRadius:"50%",backgroundImage: 'linear-gradient(to bottom right, #91defe, #99c0f9, #bdb6ec, #d7b3e3, #efb3d5,#f9bccc)' }}>
+                            <p style={{ marginTop: '50px', fontSize: "25px", fontWeight: 'bold', color: "rgb(115 10 115 / 89%)" }}>1571</p>
+                            <p style={{ marginBottom: '25px', fontSize: "25px", fontWeight: 'bold', color: "rgb(115 10 115 / 89%)" }}>Projects Completed</p>
+                        </div>
+                        <div className='col-sm' style={{margin:'50px', width:'200px', boxShadow:'1px 1px 2px 2px lightpink',height:"200px",border: "1px solid lightpink", borderRadius:"50%",backgroundImage: 'linear-gradient(to bottom right, #91defe, #99c0f9, #bdb6ec, #d7b3e3, #efb3d5,#f9bccc)' }}>
+                            <p style={{ marginTop: '50px', fontSize: "25px", fontWeight: 'bold', color: "rgb(115 10 115 / 89%)" }}>1571</p>
+                            <p style={{ marginBottom: '25px', fontSize: "25px", fontWeight: 'bold', color: "rgb(115 10 115 / 89%)" }}>Projects Completed</p>
+                        </div>
+                    </div>
+                    {/* <section className="ball" style={{ marginLeft: '400px' }}>
                         <p style={{ marginTop: '50px', fontSize: "15px", fontWeight: 'bold', color: "rgb(115 10 115 / 89%)" }}>1571</p>
-                        <p style={{ marginBottom: '20px', fontSize: "15px", fontWeight: 'bold', color: "rgb(115 10 115 / 89%)" }}>Projects Completed</p>
+                        <p style={{ marginBottom: '25px', fontSize: "15px", fontWeight: 'bold', color: "rgb(115 10 115 / 89%)" }}>Projects Completed</p>
                         <div className="shadow" style={{ marginTop: '50px', marginLeft: '25px' }}></div>
                     </section>
                     <section className="ball" style={{ marginLeft: '300px', marginRight: '300px' }}>
                         <p style={{ marginTop: '50px', fontSize: "15px", fontWeight: 'bold', color: "rgb(115 10 115 / 89%)" }}>1042</p>
-                        <p style={{ marginBottom: '20px', fontSize: "15px", fontWeight: 'bold', color: "rgb(115 10 115 / 89%)" }}>Happy Students</p>
+                        <p style={{ marginBottom: '25px', fontSize: "15px", fontWeight: 'bold', color: "rgb(115 10 115 / 89%)" }}>Happy Students</p>
                         <div className="shadow" style={{ marginTop: '50px', marginLeft: '25px' }}></div>
                     </section>
                     <section className="ball" style={{ marginRight: '400px' }}>
                         <p style={{ marginTop: '50px', fontSize: "15px", fontWeight: 'bold', color: "rgb(115 10 115 / 89%)" }}>15</p>
-                        <p style={{ marginBottom: '20px', fontSize: "15px", fontWeight: 'bold', color: "rgb(115 10 115 / 89%)" }}>Years Experience</p>
+                        <p style={{ marginBottom: '25px', fontSize: "15px", fontWeight: 'bold', color: "rgb(115 10 115 / 89%)" }}>Years Experience</p>
                         <div className="shadow" style={{ marginTop: '50px', marginLeft: '25px' }}></div>
-                    </section>
+                    </section> */}
                 </section>
             </section>
         </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent" >
                     <div className='container'></div>
                     <div className="form-inline my-2 my-lg-0">
-                        <ul className="navbar-nav ml-auto" style={{ fontSize: "25px", marginRight:'100px' }}>
+                        <ul className="navbar-nav ml-auto" style={{ fontSize: "25px" }}>
                             <li className="nav-item active">
                                 <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
                             </li>
