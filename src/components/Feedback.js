d59df8f4387db0bc506fafbcd71585eb1e7e99bb
GeForceGroot <tiwarishreyansh007@gmail.com>
2024-04-02 09:57:27 +0530

    commit 3

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -8,7 +8,7 @@ const Feedback = () => {
             <div className='text-center'>
                 <h1 style={{ fontSize: '50px', color: '#c75ec7' }}>We Are here to help you!</h1>
                 <p style={{ fontSize: '25px', }}>Assignment Help for Students on All Subjects.</p>
-                <a href="/all-subject">
+                <a href="/all-subject" target='_blank'>
                     <button id='btnR' className='btn btn-lg'>Know More...</button>
                 </a>
             </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,10 +13,10 @@ const Navbar = () => {
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent" >
                     <div className='container'></div>
                     <div className="form-inline my-2 my-lg-0">
-                        <ul className="navbar-nav ml-auto" style={{ fontSize: "25px" }}>
+                        <ul className="navbar-nav ml-auto" style={{ fontSize: "25px", marginRight:'100px' }}>
                             <li className="nav-item active">
                                 <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                             </li>
